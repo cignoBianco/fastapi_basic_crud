d088d95e3096046183fce695c3235ef20bfe522e
cignoBianco <smiana123@gmail.com>
2024-09-08 00:43:45 +0300

    raise exception

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 
 app = FastAPI()
 
 items = []
 
+
 @app.get("/")
 def root():
     return {"Hello": "World"}
+
 
 @app.post("/items")
 def create_item(item: str):
     items.append(item)
     return items
 
+
 @app.get("/items")
 def get_item():
     return items
 
+
 @app.get("/items/{item_id}")
 def get_item(item_id: int) -> str:
-    item = items[item_id]
-    return item
+    if item_id < len(items):
+        return items[item_id]
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"item id {item_id} not found. ")
+
 
